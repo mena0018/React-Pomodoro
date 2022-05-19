abbc9f8007a685cebd97bb1db754fc9b99834afe
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-05-19 15:14:15 +0200

    Ajout de la logique

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,8 @@ import "../Styles/css/Home.css"
 import Play from "../Images/play.svg"
 import Pause from "../Images/pause.svg"
 import Reset from "../Images/reset.svg"
-import {useState} from "react"
+import {useState, useEffect, useReducer} from "react"
+
 
 export default function Home() {
 
@@ -11,7 +12,44 @@ export default function Home() {
   const [breakTime, setBreakTime] = useState(300)
   const [breakTimeFixed, setBreakTimeFixed] = useState(300)
   const [chrono, setChrono] = useState(false)
- 
+  const [state, dispatch] = useReducer(reducer);
+  
+
+  useEffect(() => {
+    let id;
+
+    if (chrono) {
+      id =  window.setInterval(() => {
+        dispatch({ type: 'TICK' })
+      }, 1000)
+    }
+
+    return () => {
+      window.clearInterval(id)
+    }
+
+  }, [chrono])
+
+  function reducer (state, action) {
+    switch(action.type) {
+      case 'TICK':
+
+        if (sessionTime >= 0) {
+          setSessionTime(sessionTime - 1)
+        }
+        else if (breakTime >= 1) {
+          setBreakTime(breakTime - 1)
+        }
+        else if (sessionTime <= 0 && breakTime <= 0) {
+          setSessionTime(sessionTimeFixed)
+          setBreakTime(breakTimeFixed)
+        }
+      }
+    }
+    
+  const playPause = () => setChrono(!chrono);
+    
+
   return (
     <>
       <div className="container">          
@@ -32,21 +70,29 @@ export default function Home() {
         </div>
 
 
-        <div className="timer">25 : 00</div>
+        <div className="timer">
+          { sessionTime >= 0 ? 
+
+            `${Math.trunc(sessionTime / 60)} :  
+             ${sessionTime % 60 < 10 ? `0${sessionTime % 60}` : `${sessionTime % 60}` }`
+          : 
+            `${Math.trunc(breakTime / 60)} :  
+             ${breakTime % 60 < 10 ? `0${breakTime % 60}` : `${breakTime % 60}` }`
+          }
+        </div>
 
 
         <div className="container-buttons">
-          <div className="play">
-            <button className="play-btn">
-                <img src={Play} alt="Bouton play" />
+          <div className="play-pause">
+            <button className="play-pause-btn" onClick={playPause}>
+                <img src={chrono ? Pause : Play} alt="Bouton play / pause" />
             </button>
           </div>
-          <div className="reset-pause">
-            <button className="reset-pause-btn">
-              <img src={Reset} alt="bouton reset / pause" />
+          <div className="reset">
+            <button className="reset-btn">
+              <img src={Reset} alt="bouton reset" />
             </button>
           </div>
-
         </div>
       </div>
     </>
